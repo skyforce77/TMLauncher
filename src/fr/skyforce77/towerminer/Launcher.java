f0ea0818b18bfe18dc9fc49d05289e55330bb3c5
arnaud <arnaud@arnaud-Aspire-M7811>
2013-11-26 18:56:53 +0100

    Update 5: windows update

--- src/fr/skyforce77/towerminer/Launcher.java
+++ src/fr/skyforce77/towerminer/Launcher.java
@@ -5,9 +5,11 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Method;
 import java.net.URL;
+import java.net.URLClassLoader;
+
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
@@ -16,9 +18,10 @@ public class Launcher extends JFrame{
 	private static final long serialVersionUID = -3444205831495972681L;
 	public static String versionurl = "http://dl.dropboxusercontent.com/u/38885163/TowerMiner/version/version.txt";
 	public static String downloadurl = "http://dl.dropboxusercontent.com/u/38885163/TowerMiner/version/TowerMiner.jar";
-	public static int version = 4;
+	public static int version = 5;
 	public static Launcher instance;
 	public static String actual = "";
+	public static String actualdesc = "";
 
 	public static void main(String[] args) {
 		instance = new Launcher();
@@ -31,7 +34,7 @@ public class Launcher extends JFrame{
 			getGame().getParentFile().mkdirs();
 			String version = getVersion();
 			if(!version.equals(actual)) {
-				if(!Download.update("Mise à jour en cours...", actual.replace(" ",""))) {
+				if(!Download.update("Mise à jour en cours...", actual+"\n"+actualdesc)) {
 					return;
 				}
 				setVersion(actual);
@@ -47,10 +50,17 @@ public class Launcher extends JFrame{
 			JOptionPane.showMessageDialog(instance, "Téléchargement du jeu effectué, vous devez maintenant relancer le jeu","Information",JOptionPane.INFORMATION_MESSAGE);
 		}
 		try {
-			if(!arg.equals("install"))
-				Runtime.getRuntime().exec("java -jar "+getGame()+" "+version+" "+arg+" "+getOS());
-		} catch (IOException e) {
+			if(!arg.equals("install")) { 
+				URL[] urls = { getGame().toURI().toURL() };  
+				@SuppressWarnings("resource")
+				URLClassLoader loader = new URLClassLoader(urls);  
+				Class<?> cls = loader.loadClass("fr.skyforce77.towerminer.TowerMiner");
+				Method main = cls.getDeclaredMethod("startGame", int.class, String.class, String.class);
+				main.invoke(cls.newInstance(), new Object[]{version,arg,getOS()});
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
+			JOptionPane.showMessageDialog(instance, "Une erreur est survenue,\nelle peut être causée par une mise à jour du launcher requise.","Information",JOptionPane.ERROR_MESSAGE);
 		}
 		instance.dispose();
 	}
@@ -66,7 +76,7 @@ public class Launcher extends JFrame{
 			return new File(System.getProperty("user.home"),"/.towerminer");
 		return new File(System.getProperty("user.dir"),"/.towerminer");
 	}
-	
+
 	public static String getOS() {
 		String OS = System.getProperty("os.name").toUpperCase();
 		if (OS.contains("WIN"))
@@ -120,12 +130,11 @@ public class Launcher extends JFrame{
 	}
 
 	public static boolean getActualVersion() {
-		String version = "";
 		try {
 			BufferedReader out = new BufferedReader(new InputStreamReader(new URL(versionurl).openStream()));
-			version = out.readLine();
+			actual = out.readLine();
+			actualdesc = out.readLine();
 			out.close();
-			actual = version;
 			return true;
 		}catch (Exception e){
 			return false;
